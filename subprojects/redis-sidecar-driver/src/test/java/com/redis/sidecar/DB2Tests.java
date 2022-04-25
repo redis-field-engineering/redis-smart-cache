68106bd76d6bcd7aa0bfc5679f4ef505c6bc6cf4
Julien Ruaux <julien@c64.arcillaux.lan>
2022-04-25 16:44:11 -0700

    Added DB2 tests

--- subprojects/redis-sidecar-driver/src/test/java/com/redis/sidecar/DB2Tests.java
+++ subprojects/redis-sidecar-driver/src/test/java/com/redis/sidecar/DB2Tests.java
@@ -1,5 +1,34 @@
 package com.redis.sidecar;
 
-public class DB2Tests {
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.testcontainers.containers.Db2Container;
+import org.testcontainers.junit.jupiter.Container;
+
+import com.redis.testcontainers.junit.RedisTestContext;
+import com.redis.testcontainers.junit.RedisTestContextsSource;
+
+class DB2Tests extends AbstractSidecarTests {
+
+	@SuppressWarnings("deprecation")
+	@Container
+	private static final Db2Container DB2 = new Db2Container().acceptLicense();
+
+	@BeforeAll
+	public void setupAll() throws SQLException, IOException {
+		Connection backendConnection = connection(DB2);
+		runScript(backendConnection, "db2/create.sql");
+		runScript(backendConnection, "db2/data.sql");
+	}
+	
+	@ParameterizedTest
+	@RedisTestContextsSource
+	void testSimpleStatement(RedisTestContext redis) throws Exception {
+		testSimpleStatement(DB2, redis, "SELECT * FROM books");
+	}
 
 }
---
+++ subprojects/redis-sidecar-driver/src/test/resources/db2/drop.sql
@@ -0,0 +1,6 @@
+DROP TABLE book_authors;
+DROP TABLE book_genres;
+DROP TABLE authors;
+DROP TABLE books;
+DROP TABLE genres;
+DROP TABLE publishers;
