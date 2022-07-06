a0a0de255fc9116f252bbc958e1d2fa8e611e381
Julien Ruaux <jruaux@c64.arcillaux.local>
2022-07-06 13:52:53 -0700

    fix: @param name

--- core/redis-sidecar-jdbc/src/main/java/com/redis/sidecar/core/ResultSetCache.java
+++ core/redis-sidecar-jdbc/src/main/java/com/redis/sidecar/core/ResultSetCache.java
@@ -8,7 +8,7 @@ public interface ResultSetCache {
 
 	/**
 	 * 
-	 * @param key the key to get the ResultSet for.
+	 * @param sql SQL statement to get the ResultSet for.
 	 * @return optional with the ResultSet that was retrieved from cache or empty
 	 *         optional if none found.
 	 * @throws SQLException if the ResultSet could not be retrieved
@@ -18,7 +18,7 @@ public interface ResultSetCache {
 	/**
 	 * Adds a ResultSet to the cache.
 	 *
-	 * @param key       the key to store the ResultSet under.
+	 * @param sql       SQL statement to store the ResultSet under.
 	 * @param ttl       the key TTL in seconds.
 	 * @param resultSet the ResultSet to store under the key.
 	 * @throws SQLException if an error occurred while storing the ResultSet
