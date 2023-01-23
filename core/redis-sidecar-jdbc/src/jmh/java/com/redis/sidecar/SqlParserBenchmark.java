5505f9af211aea8cf5234599c53e4a904db96c58
Julien Ruaux <jrx@github.com>
2023-01-23 15:43:58 -0800

    test: Added benchmark tests

--- core/redis-sidecar-jdbc/src/jmh/java/com/redis/sidecar/SqlParserBenchmark.java
+++ core/redis-sidecar-jdbc/src/jmh/java/com/redis/sidecar/SqlParserBenchmark.java
@@ -6,10 +6,16 @@ import org.openjdk.jmh.annotations.Mode;
 
 import net.sf.jsqlparser.JSQLParserException;
 
-public class ParserBenchmark {
+public class SqlParserBenchmark {
 
 	private static final SqlParser PARSER = new SqlParser();
 	private static final String SQL = "SELECT SLEEP(1), orders.orderNumber, orders.orderDate, orders.requiredDate, orders.shippedDate, orders.status, orders.customerNumber, customers.customerName, orderdetails.productCode, products.productName, orderdetails.quantityOrdered FROM orders JOIN customers ON orders.customerNumber = customers.customerNumber JOIN orderdetails ON orders.orderNumber = orderdetails.orderNumber JOIN products ON orderdetails.productCode = products.productCode WHERE orders.orderNumber = ?";
+
+	@Benchmark
+	@BenchmarkMode(Mode.AverageTime)
+	public void parse() throws JSQLParserException {
+		PARSER.parse(SQL);
+	}
 
 	@Benchmark
 	@BenchmarkMode(Mode.AverageTime)
