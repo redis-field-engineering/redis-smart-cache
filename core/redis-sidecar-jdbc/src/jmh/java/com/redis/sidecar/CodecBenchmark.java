420ee64647c2b116ca8d7ae79da0108583cbc100
Julien Ruaux <jrx@github.com>
2023-01-24 09:28:03 -0800

    test: Added result-set encoding benchmarks

--- core/redis-sidecar-jdbc/src/jmh/java/com/redis/sidecar/CodecBenchmark.java
+++ core/redis-sidecar-jdbc/src/jmh/java/com/redis/sidecar/CodecBenchmark.java
@@ -9,21 +9,21 @@ import org.openjdk.jmh.annotations.Mode;
 
 public class CodecBenchmark {
 
-//	@Benchmark
-//	@BenchmarkMode(Mode.AverageTime)
-//	public void explicitEncode(CodecExecutionPlan plan) throws SQLException {
-//		plan.getExplicitCodec().encodeValue(plan.newRowSet()).array();
-//	}
-//
-//	@Benchmark
-//	@BenchmarkMode(Mode.AverageTime)
-//	public void jdkEncode(CodecExecutionPlan plan) throws IOException, SQLException {
-//		plan.getJdkCodec().encode(plan.newRowSet());
-//	}
+	@Benchmark
+	@BenchmarkMode(Mode.AverageTime)
+	public void explicitEncode(CodecExecutionPlan plan) throws SQLException {
+		plan.getExplicitCodec().encodeValue(plan.newRowSet()).array();
+	}
 
 	@Benchmark
 	@BenchmarkMode(Mode.AverageTime)
-	public void explicitDecode(CodecExecutionPlan plan) throws IOException, SQLException, ClassNotFoundException {
+	public void jdkEncode(CodecExecutionPlan plan) throws IOException, SQLException {
+		plan.getJdkCodec().encode(plan.newRowSet());
+	}
+
+	@Benchmark
+	@BenchmarkMode(Mode.AverageTime)
+	public void explicitDecode(CodecExecutionPlan plan) {
 		plan.getExplicitCodec().decodeValue(plan.getExplicitByteBufer());
 	}
 
