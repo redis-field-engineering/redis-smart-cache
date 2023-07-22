c52a475765f627b0ac63117fbf25f608997ef9e9
Julien Ruaux <jruaux@users.noreply.github.com>
2023-07-21 23:34:23 -0600

    feat: handling get and put (OOM) exceptions.
    Resolves #22 #24

--- core/redis-smart-cache-jdbc/src/main/java/com/redis/smartcache/jdbc/RedisRowSetCache.java
+++ core/redis-smart-cache-jdbc/src/main/java/com/redis/smartcache/jdbc/RedisRowSetCache.java
@@ -1,43 +1,72 @@
 package com.redis.smartcache.jdbc;
 
+import java.time.Duration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.sql.RowSet;
 
+import com.redis.lettucemod.api.StatefulRedisModulesConnection;
 import com.redis.lettucemod.util.RedisModulesUtils;
 
 import io.lettuce.core.AbstractRedisClient;
-import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.RedisException;
 import io.lettuce.core.codec.RedisCodec;
 
 public class RedisRowSetCache implements RowSetCache {
 
-	private final StatefulRedisConnection<String, RowSet> connection;
+    private static final Logger log = Logger.getLogger(RedisRowSetCache.class.getName());
 
-	public RedisRowSetCache(AbstractRedisClient client, RedisCodec<String, RowSet> codec) {
-		this.connection = RedisModulesUtils.connection(client, codec);
-	}
+    private static final String ERROR_OOM = "OOM command not allowed";
 
-	@Override
-	public RowSet get(String key) {
-		if (connection.isOpen()) {
-			return connection.sync().get(key);
-		}
-		return null;
-	}
+    private final StatefulRedisModulesConnection<String, RowSet> connection;
 
-	@Override
-	public void put(String key, RowSet rowSet, long ttlMillis) {
-		if (connection.isOpen()) {
-			if (ttlMillis > 0) {
-				connection.sync().psetex(key, ttlMillis, rowSet);
-			} else {
-				connection.sync().set(key, rowSet);
-			}
-		}
-	}
+    private final long oomRetryInterval;
 
-	@Override
-	public void close() {
-		connection.close();
-	}
+    private long nextOOMCheck;
+
+    public RedisRowSetCache(AbstractRedisClient client, RedisCodec<String, RowSet> codec, Duration oomRetryInterval) {
+        this.connection = RedisModulesUtils.connection(client, codec);
+        this.oomRetryInterval = oomRetryInterval.toMillis();
+    }
+
+    @Override
+    public RowSet get(String key) {
+        if (connection.isOpen()) {
+            return connection.sync().get(key);
+        }
+        return null;
+    }
+
+    @Override
+    public void put(String key, RowSet rowSet, long ttlMillis) {
+        long currentTimeMillis = System.currentTimeMillis();
+        if (connection.isOpen() && currentTimeMillis > nextOOMCheck) {
+            try {
+                doPut(key, rowSet, ttlMillis);
+            } catch (RedisException e) {
+                String message = e.getMessage();
+                if (message != null && message.startsWith(ERROR_OOM)) {
+                    log.log(Level.SEVERE, "Redis server out of memory. Puts disabled until memory is available.", e);
+                    nextOOMCheck = System.currentTimeMillis() + oomRetryInterval;
+                } else {
+                    log.log(Level.SEVERE, "Could not put rowset in cache", e);
+                }
+            }
+        }
+    }
+
+    private void doPut(String key, RowSet rowSet, long ttlMillis) {
+        if (ttlMillis > 0) {
+            connection.sync().psetex(key, ttlMillis, rowSet);
+        } else {
+            connection.sync().set(key, rowSet);
+        }
+    }
+
+    @Override
+    public void close() {
+        connection.close();
+    }
 
 }
