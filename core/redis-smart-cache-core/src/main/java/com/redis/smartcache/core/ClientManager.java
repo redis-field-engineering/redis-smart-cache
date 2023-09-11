3b2937deb94220d799ed39d4f1e281ac3ee088ed
Julien Ruaux <jruaux@users.noreply.github.com>
2023-09-11 15:29:37 -0700

    style: Removed duplicate if statement

--- core/redis-smart-cache-core/src/main/java/com/redis/smartcache/core/ClientManager.java
+++ core/redis-smart-cache-core/src/main/java/com/redis/smartcache/core/ClientManager.java
@@ -1,16 +1,18 @@
 package com.redis.smartcache.core;
 
-import com.redis.lettucemod.RedisModulesClient;
-import com.redis.lettucemod.cluster.RedisModulesClusterClient;
-import com.redis.smartcache.core.config.RedisConfig;
-import io.lettuce.core.AbstractRedisClient;
-import io.lettuce.core.RedisURI;
-import org.springframework.util.StringUtils;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import org.springframework.util.StringUtils;
+
+import com.redis.lettucemod.RedisModulesClient;
+import com.redis.lettucemod.cluster.RedisModulesClusterClient;
+import com.redis.smartcache.core.config.RedisConfig;
+
+import io.lettuce.core.AbstractRedisClient;
+import io.lettuce.core.RedisURI;
 
 public class ClientManager implements AutoCloseable {
 
@@ -23,33 +25,28 @@ public class ClientManager implements AutoCloseable {
     }
 
     private AbstractRedisClient createClient(RedisConfig config) {
-        RedisURI redisURI = buildRedisURI(config);
-        log.log(Level.FINE, "Creating Redis client with URI {0}", redisURI);
-        if(config.isCluster()){
+        RedisURI redisURI = redisURI(config);
+        log.log(Level.FINE, "Creating Redis client with URI {0} and Cluster {1}",
+                new Object[] { redisURI, config.isCluster() });
+        if (config.isCluster()) {
             return RedisModulesClusterClient.create(redisURI);
         }
-        else{
-            return RedisModulesClient.create(redisURI);
-        }
+        return RedisModulesClient.create(redisURI);
     }
 
-    private RedisURI buildRedisURI(RedisConfig config){
-        RedisURI startingUri = RedisURI.create(config.getUri());
-
-        RedisURI.Builder builder = RedisURI.builder(startingUri);
-
-        if(StringUtils.hasLength(config.getUsername()) && config.getPassword() != null && config.getPassword().length > 0){
-            builder.withAuthentication(config.getUsername(), config.getPassword());
+    private RedisURI redisURI(RedisConfig config) {
+        RedisURI.Builder builder = RedisURI.builder(RedisURI.create(config.getUri()));
+        if (config.getPassword() != null && config.getPassword().length > 0) {
+            if (StringUtils.hasLength(config.getUsername())) {
+                builder.withAuthentication(config.getUsername(), config.getPassword());
+            } else {
+                builder.withPassword(config.getPassword());
+            }
         }
-        else if(config.getPassword() != null && config.getPassword().length > 0){
-            builder.withPassword(config.getPassword());
-        }
-
-        if(config.isTls()){
+        if (config.isTls()) {
             builder.withSsl(config.isTls());
             builder.withVerifyPeer(config.getTlsVerify());
         }
-
         return builder.build();
     }
 
